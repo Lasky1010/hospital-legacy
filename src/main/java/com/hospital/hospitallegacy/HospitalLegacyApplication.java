81e522d821bc6be4613940a70f06c1365361b63e
Alexander <kovgunovalexander@gmail.com>
2024-11-07 17:28:45 +0300

    init commit

---
+++ src/main/java/com/hospital/hospitallegacy/HospitalLegacyApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hospitallegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalLegacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalLegacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/hospitallegacy/HospitalLegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospitallegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalLegacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
